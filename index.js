68a99a6dc8ce42e8f6c0353c5bf232b7134fc660
Osseum1 <VITIN@EMAIL.com>
2019-04-23 00:28:37 +0300

    реализованы:   цвет   разный размер   мембрана   разная масса объектов (кроме шаров)   гравитация

--- index.js
+++ index.js
@@ -1,141 +1,358 @@
 let canvasHeight;
 let canvasWidth;
 
-let height;
-let width;
-
-
 let myImageData;
 
-const scale = 1;
+const comparator = (x, y) => x.r.y - y.r.y;
+const E = l => l.map(x => x.m * x.v.lengthSq()).reduce((acc, value) => acc + value) / 2;
+const p = l => l.map(x => x.v.clone().multiplyScalar(x.m)).reduce((acc, value) => acc.add(value), new Victor(0, 0));
 
 init();
 
 function init() {
 
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
 
-  canvasHeight = canvas.height;
-  canvasWidth = canvas.width;
+    canvasHeight = canvas.height;
+    canvasWidth = canvas.width;
 
-  console.log(canvasWidth, canvasHeight);
+    console.log(canvasWidth, canvasHeight);
 
-  myImageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-  const {data} = myImageData;
+    myImageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
-  height = canvasHeight / scale;
-  width = canvasWidth / scale;
-
-  redraw(ctx);
+    initScene(ctx);
 }
 
-function newArr(h, w) {
-  let arr1 = new Array(h);
-  for (let i = 0; i < h; i++) {
-    arr1[i] = new Array(w);
-  }
-  return arr1;
-}
+function initScene(ctx) {
+    canvasHeight = canvas.height;
+    canvasWidth = canvas.width;
 
-function redraw(ctx) {
-  canvasHeight = canvas.height;
-  canvasWidth = canvas.width;
+    const g1 = new Victor(canvasWidth / 2, canvasHeight / 20);
+    const v1 = new Victor(0.1, 1);
+    const ball1 = new Ball(g1, v1, "blue");
 
-  const g1 = new Victor(450, 50);
-  const v1 = new Victor(0, 0);
-  const ball1 = new Ball(g1, v1);
+    // const scene = [];
+    const scene = [ball1];
 
-  const g2 = new Victor(350, 400);
-  const v2 = new Victor(-2.5, 1);
-  const ball2 = new Ball(g2, v2);
+    while (scene.length < 1) {
+        const x = new Victor(0, 0).randomize(new Victor(0, 0), new Victor(canvasWidth, canvasHeight));
+        const v = new Victor(0, 0).randomize(new Victor(-1, 1), new Victor(1, -1));
 
-  const scene = [ball2];
+        let ball = new Ball(x, v);
 
-  loop(ctx, scene);
+        ball.color = x.y > canvasHeight / 2 ? "red" : "blue";
+        ball.m = x.y > canvasHeight / 2 ? 2 : 1;
+
+        for (const movable of scene) {
+            if (movable.r.distanceSq(ball.r) <= 4 * movable.R * ball.R) {
+                ball = null;
+                break;
+            }
+        }
+        if (ball)
+            scene.push(ball);
+    }
+
+    scene.push(new Membrane(new Victor(canvasWidth / 2, canvasHeight * 0.4), new Victor(0, 0), "blue"));
+
+    loop(ctx, scene);
 
 }
 
 function loop(ctx, scene) {
-  const now = new Date();
 
-  const dt = 1.1;
+    const now = new Date();
 
-  for (const movable of scene) {
-    const dr = movable.v.clone();
-    dr.multiplyScalar(dt);
-    movable.r.add(dr);
-  }
+    const dt = 1;
+    const g = 0;
 
-  for (const movable of scene) {
-    if (movable.r.x < 0) {
-      movable.r.x = -movable.r.x;
-      movable.v.x = -movable.v.x;
+    scene.sort(comparator);
+
+    for (const movable of scene) {
+        movable.v.addScalarY(g * dt);
+
+        const dr = movable.v.clone();
+        dr.multiplyScalar(dt);
+        movable.r.add(dr);
     }
 
-    if (movable.r.x >= canvasWidth) {
-      movable.r.x = canvasWidth - (movable.r.x - canvasWidth);
-      movable.v.x = -movable.v.x;
+    function intersectBallWithMembrane(ball, membrane) {
+        return ball.color === membrane.color && Math.abs(ball.r.y - membrane.r.y) < ball.R + membrane.R;
     }
 
-    if (movable.r.y < 0) {
-      movable.r.y = -movable.r.y;
-      movable.v.y = -movable.v.y;
+    function intersectBalls(b1, b2) {
+        const r12 = b1.R + b2.R;
+        return b1.r.distanceSq(b2.r) <= r12 * r12;
     }
 
-    if (movable.r.y >= canvasHeight) {
-      movable.r.y = canvasHeight - (movable.r.y - canvasHeight);
-      movable.v.y = -movable.v.y;
+    for (let i = 0; i < scene.length; i++) {
+        const movable = scene[i];
+
+        collideWithBorder(movable);
+
+        for (let j = i; j < scene.length; j++) {
+            const otherMovable = scene[j];
+
+            if (movable.r.y - otherMovable.r.y > movable.R + otherMovable.R)
+                break;
+
+            if (movable === otherMovable)
+                continue;
+
+            if (Ball.prototype.isPrototypeOf(movable)) {
+                if (Ball.prototype.isPrototypeOf(otherMovable)) {
+                    if (intersectBalls(movable, otherMovable))
+                        collideBalls(otherMovable, movable, dt);
+
+                } else {
+                    if (intersectBallWithMembrane(movable, otherMovable))
+                        collideBallWithMembrane(movable, otherMovable, dt);
+                }
+            } else {
+                if (intersectBallWithMembrane(otherMovable, movable))
+                    collideBallWithMembrane(otherMovable, movable, dt);
+            }
+        }
     }
-  }
 
-  for (const drawable of scene)
-    drawBall(drawable, myImageData);
+    console.log(`E= ${E(scene)}`);
 
+    const {data} = myImageData;
+    for (let i = 0; i < canvasWidth; i++) {
+        for (let j = 0; j < canvasHeight; j++) {
+            data[(j * canvasWidth + i) * 4] = 255;
+            data[(j * canvasWidth + i) * 4 + 1] = 255;
+            data[(j * canvasWidth + i) * 4 + 2] = 255;
+            data[(j * canvasWidth + i) * 4 + 3] = 255;
+        }
+    }
 
-  ctx.putImageData(myImageData, 0, 0);
-  // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    for (const drawable of scene)
+        if (Ball.prototype.isPrototypeOf(drawable))
+            drawBall(drawable, myImageData);
+        else
+            drawMembrane(drawable, myImageData);
 
-  setTimeout(loop, Math.max(0, 100 + new Date() - now), ctx, scene);
+    ctx.putImageData(myImageData, 0, 0);
+
+    setTimeout(loop, Math.max(0, 100 + new Date() - now), ctx, scene);
+
+}
+
+function touchObjects(m1, m2, c) {
+    const v1_before_M = m1.v.clone().subtract(m2.v); // скорость мяча в СО мембраны
+    const v2_before_M = m2.v.clone().subtract(m2.v); // скорость мемб в СО мембраны (всегда 0)
+
+    const vn1_before_M = v1_before_M.dot(c);
+    const vn2_before_M = v2_before_M.dot(c); // (всегда 0)
+
+    const b_mult = (m1.m - m2.m) / (m1.m + m2.m);
+    const m_mult = 2 * m1.m / (m1.m + m2.m);
+
+    const vn1_after_M = c.clone().multiplyScalar(vn1_before_M * b_mult); // нормальная скорость мяча после столкновения в СО мембраны
+    const vn2_after_M = c.clone().multiplyScalar(vn1_before_M * m_mult); // нормальная скорость мемб после столкновения в СО мембраны
+
+    const vn1_after_I = vn1_after_M.add(m2.v); // нормальная скорость мяча после столкновения в неподвижной СО
+    const vn2_after_I = vn2_after_M.add(m2.v); // нормальная скорость мемб после столкновения в неподвижной СО
+
+    const dv1 = c.clone().multiplyScalar(vn1_before_M * b_mult - vn1_before_M); // приращение скорости мяча после столкновения
+    const dv2 = c.clone().multiplyScalar(vn1_before_M * m_mult - vn2_before_M); // приращение скорости мемб после столкновения
+    return {dv1, dv2};
+}
+
+function collideBalls(b1, b2, dt) {
+    const dr = b2.r.clone().subtract(b1.r);
+    const c = dr.clone().norm();
+
+    const vn1i = c.dot(b1.v);
+    const vn2i = c.dot(b2.v);
+
+    let t = (dr.length() - b1.R - b2.R ) / (vn1i - vn2i);
+    if (isNaN(t) || !isFinite(t)) {
+        t = 0;
+    }
+    {
+        const dr1 = b1.v.clone();
+        dr1.multiplyScalar(t);
+        b1.r.add(dr1);
+    }
+    {
+        const dr2 = b2.v.clone();
+        dr2.multiplyScalar(t);
+        b2.r.add(dr2);
+    }
+
+    const {dv1, dv2} = touchObjects(b1, b2, c);
+
+    b1.v.add(dv1);
+    b2.v.add(dv2);
+
+    {
+        const dr = b1.v.clone();
+        dr.multiplyScalar(dt - t);
+        b1.r.add(dr);
+    }
+    {
+        const dr = b2.v.clone();
+        dr.multiplyScalar(dt - t);
+        b2.r.add(dr);
+    }
+}
+
+function collideWithBorder(ball) {
+    if (ball.r.x < 0) {
+        ball.r.x = -ball.r.x;
+        ball.v.x = -ball.v.x;
+    }
+
+    if (ball.r.x >= canvasWidth) {
+        ball.r.x = canvasWidth - (ball.r.x - canvasWidth);
+        ball.v.x = -ball.v.x;
+    }
+
+    if (ball.r.y < 0) {
+        ball.r.y = -ball.r.y;
+        ball.v.y = -ball.v.y;
+    }
+
+    if (ball.r.y >= canvasHeight) {
+        ball.r.y = canvasHeight - (ball.r.y - canvasHeight);
+        ball.v.y = -ball.v.y;
+    }
+}
+
+function getCollisionLine(b1, b2) {
+    const dr = b1.r.clone().subtract(b2.r);
+
+    if (!Ball.prototype.isPrototypeOf(b1) || !Ball.prototype.isPrototypeOf(b2))
+        dr.x = 0;
+
+    return dr.clone().norm();
+}
+
+function collideBallWithMembrane(b1, b2, dt) {
+    const p1 = p([b1, b2]);
+    const E1 = E([b1, b2]);
+
+    const c = getCollisionLine(b1, b2);
+
+    const dr = b1.r.clone().subtract(b2.r);
+    const distance = Math.abs(c.dot(dr));
+
+    // перемещаем объекты к точке столкновения
+    const vn1 = c.dot(b1.v);
+    const vn2 = c.dot(b2.v);
+
+    let t = (distance - b1.R - b2.R) / (vn2 - vn1);
+    if (isNaN(t) || !isFinite(t)) {
+        t = 0;
+    }
+    {
+        const dr1 = b1.v.clone();
+        dr1.multiplyScalar(t);
+        b1.r.add(dr1);
+    }
+    {
+        const dr2 = b2.v.clone();
+        dr2.multiplyScalar(t);
+        b2.r.add(dr2);
+    }
+
+    const {dv1, dv2} = touchObjects(b1, b2, c);
+
+    b1.v.add(dv1);
+    b2.v.add(dv2);
+
+    { // пересчитываем перемещение после столкновения по новым скоростям
+        const dr = b2.v.clone();
+        dr.multiplyScalar(dt - t);
+        b2.r.add(dr);
+    }
+    {
+        const dr = b1.v.clone();
+        dr.multiplyScalar(dt - t);
+        b1.r.add(dr);
+    }
+
+    console.log(` dE  = ${E1 - E([b1, b2])}`);
+    console.log(`|dp| = ${p1.subtract(p([b1, b2])).lengthSq()}`);
 }
 
 function drawBall(ball, imgData) {
-  const R = ball.R;
-  const sqR = R * R;
-  const t = new Victor(0, 0);
+    const R = ball.R;
+    const sqR = R * R;
+    const t = new Victor(0, 0);
 
-  const {data} = imgData;
-  let color;
+    const {data} = imgData;
+    let color;
 
-  for (let i = -R; i <= R; i++) {
-    for (let j = -R; j <= R; j++) {
-      t.x = i;
-      t.y = j;
-      color = [255, 255, 255, 255];
-      if (t.lengthSq() <= sqR) {
-        t.add(ball.r);
-        t.unfloat();
+    for (let i = -R; i <= R; i++) {
+        for (let j = -R; j <= R; j++) {
+            t.x = i;
+            t.y = j;
+            color = [255, 255, 255, 255];
+            if (t.lengthSq() <= sqR) {
+                t.add(ball.r);
+                t.unfloat();
 
-        if (checkInCanvas(t, canvasWidth, canvasHeight)) {
-          color = [255, 0, 0, 255];
+                if (checkInCanvas(t, canvasWidth, canvasHeight)) {
+                    color = ball.color === "red" ? [255, 0, 0, 255] : [0, 0, 255, 255];
 
-          data[(t.y * canvasWidth + t.x) * 4] = color[0];
-          data[(t.y * canvasWidth + t.x) * 4 + 1] = color[1];
-          data[(t.y * canvasWidth + t.x) * 4 + 2] = color[2];
-          data[(t.y * canvasWidth + t.x) * 4 + 3] = color[3];
+                    data[(t.y * canvasWidth + t.x) * 4] = color[0];
+                    data[(t.y * canvasWidth + t.x) * 4 + 1] = color[1];
+                    data[(t.y * canvasWidth + t.x) * 4 + 2] = color[2];
+                    data[(t.y * canvasWidth + t.x) * 4 + 3] = color[3];
+                }
+            }
         }
-      }
     }
-  }
+}
 
+function drawMembrane(memb, imgData) {
+    const R = memb.R;
+    const t = new Victor(0, 0);
+
+    const {data} = imgData;
+    let color;
+
+    for (let i = -canvasWidth / 2; i <= canvasWidth / 2; i++) {
+        for (let j = -R; j <= R; j++) {
+            t.x = i;
+            t.y = j;
+            color = [255, 255, 255, 255];
+
+            t.add(memb.r);
+            t.unfloat();
+
+            if (checkInCanvas(t, canvasWidth, canvasHeight)) {
+                color = memb.color === "red" ? [255, 0, 0, 255] : [0, 0, 255, 255];
+
+                data[(t.y * canvasWidth + t.x) * 4] = color[0];
+                data[(t.y * canvasWidth + t.x) * 4 + 1] = color[1];
+                data[(t.y * canvasWidth + t.x) * 4 + 2] = color[2];
+                data[(t.y * canvasWidth + t.x) * 4 + 3] = color[3];
+            }
+        }
+    }
 }
 
 function checkInCanvas(r, width, height) {
-  return r.x >= 0 && r.y > 0 && r.x < width && r.y < height;
+    return r.x >= 0 && r.y > 0 && r.x < width && r.y < height;
 }
 
-function Ball(r, v, R) {
-  this.r = r;
-  this.v = v;
-  this.R = R | 10;
+function Ball(r, v, color, R) {
+    this.r = r;
+    this.v = v;
+    this.color = color;
+    this.R = R | 3;
+    this.m = 1;
+}
+
+function Membrane(r, v, color) {
+    this.r = r;
+    this.v = v;
+    this.color = color;
+    this.R = 5;
+    this.m = 1;
 }
